795fbad260287182d96540d7254b529877b23ee3
chris norris <cnorris2007@gmail.com>
2021-12-08 21:01:27 +0000

    Formatting

--- app/src/AOC/Y2021/Day7.hs
+++ app/src/AOC/Y2021/Day7.hs
@@ -4,11 +4,16 @@ import Library
 main = main7 Full
 
 main7 source = do
-  crabPositions :: [Int] <- read . (<> "]") . ("[" <>) <$> readFile21 ("d7.input" <> show source)
-  printPart 1 $
-    minimum [ sum $ abs . subtract c <$> crabPositions | c <- [1.. maximum crabPositions]]
-  printPart 2 $
-    minimum [ sum $  (\n -> n*(n+1) `div` 2) . abs . subtract c <$> crabPositions | c <- [1.. maximum crabPositions]]
+  crabPositions :: [Int] <- read . (<> "]") . ("[" <>) <$> readFile21
+    ("d7.input" <> show source)
+  printPart 1 $ minimum
+    [ sum $ abs . subtract c <$> crabPositions
+    | c <- [1 .. maximum crabPositions]
+    ]
+  printPart 2 $ minimum
+    [ sum $ (\n -> n * (n + 1) `div` 2) . abs . subtract c <$> crabPositions
+    | c <- [1 .. maximum crabPositions]
+    ]
 
- where 
+ where
   printPart = (print .) . (. show) . (<>) . (<> " :") . ("part " <>) . show
