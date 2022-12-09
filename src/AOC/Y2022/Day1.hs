bb8d59e3b48e64a9579ae1beb531bf05a9999658
chris norris <cnorris2007@gmail.com>
2022-12-09 22:00:04 +0000

    Day 1

---
+++ src/AOC/Y2022/Day1.hs
@@ -0,0 +1,15 @@
+module AOC.Y2022.Day1 where
+
+import Library
+
+import Data.List
+
+main_pt1 :: IO Int
+main_pt1 = maximum <$> getCalories
+
+main_pt2 = sum . take 3 . reverse . sort <$> getCalories
+
+getCalories =  (calories <$> inpStr 2022 "d1.input")
+ where
+  calories =
+    map ((sum . map read) . filter (/= mempty)) . groupBy (const (/= ""))
