91121a2fa64bc4a43b5e956d3b93b57e795e6787
chris norris <cnorris2007@gmail.com>
2022-12-12 18:28:30 +0000

    Try a few more

---
+++ src/AOC/Y2022/Day4.hs
@@ -0,0 +1,28 @@
+module AOC.Y2022.Day4 where
+
+import Library hiding (fromList, parse)
+import qualified Data.Set  as S
+
+data Range a = R a a a a
+  deriving (Show, Functor)
+
+main_pt1 = do
+  x <- inpStr 2022 "d4.input"
+  let n :: [Range Int] = toRanges <$> fmap parse x
+  return $ sum (valid <$> n)
+ where
+  valid (R a b c d) = fromEnum ((a <= c) && (b <= d) || (a >= c) && (b >= d))
+
+main_pt2 :: IO Int
+main_pt2 = do
+  x <- inpStr 2022 "d4.input"
+  let n :: [Range Int] = toRanges <$> fmap parse x
+  return $ sum $ overlap <$> n
+ where
+  overlap (R a b c d) = (fromEnum . (> 0) . S.size)
+    $ S.intersection (S.fromList [a .. abs b]) (S.fromList [c .. abs d])
+
+toRanges ((a, b), (c, d)) = read <$> R a b c d
+parse = bimap breakHyphens (breakHyphens . drop 1) . breakComma
+breakHyphens = break (== '-')
+breakComma = break (== ',')
