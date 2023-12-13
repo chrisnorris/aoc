f474a6b472d1761938fb84f3c82646af602aa43f
chris norris <cnorris2007@gmail.com>
2023-12-13 19:32:13 +0000

    Day 8

--- src/AOC/Y2023/Day8.hs
+++ src/AOC/Y2023/Day8.hs
@@ -2,23 +2,36 @@ module AOC.Y2023.Day8 where
 
 import AOC.Y2021.Day24(integer)
 import Library
-import Data.Map qualified as M
+import qualified Data.Map as M
 import Data.List
 import Data.Char
 
 main_pt1 = do
   al <- lines <$> readFileY 2023 "d8.input"
-  let (Right tuples) = sequence $ parse (tuplesParser) "" <$> (drop 2 al)
+  let (Right tuples) = sequence $ parse tuplesParser "" <$> drop 2 al
   return $ loop (concat $ repeat $ head al) 0 "AAA" (M.fromList tuples)
 
  where  loop _ n "ZZZ" _ = n
         loop (i:is) n e m =
-              let r = let result = fromJust $ M.lookup e m in if i == 'L' then (head result) else (head $ drop 1 $ result) in
+              let r = let result = fromJust $ M.lookup e m in if i == 'L' then head result else result !! 1 in
               loop is (n+1) r m
+
+main_pt2 = do
+  al <- lines <$> readFileY 2023 "d8.input"
+  let (Right tuples) = sequence $ parse tuplesParser "" <$> drop 2 al
+      m = M.fromList tuples
+      starts = filter ( (=='A')  .  head . reverse ) (M.keys m)
+  return $ loop (concat $ repeat $ head al) 0 starts (M.fromList tuples)
+
+ where  loop _ n l _ | (finished l) = n
+        loop (i:is) n l m =
+              loop is (n+1) ((\e -> let result = fromJust $ M.lookup e m in if i == 'L' then head result else result !! 1) <$> l) m
 
 tuplesParser = do 
   k <- many1 upper; many (noneOf "(")
-  t <- between (char '(') (char ')') ( many1 (upper <|> space ) `sepBy` (char ','))
+  t <- between (char '(') (char ')') ( many1 (upper <|> space ) `sepBy` char ',')
   return (k, trim <$> t)
 
 trim = dropWhileEnd isSpace . dropWhile isSpace
+
+finished = all ( (== 'Z') . last)
