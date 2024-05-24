33cc5f48b9ec2f14b5bbf97b9f5c2ad5b7c31725
chrisnorris <chrisnorris@users.noreply.github.com>
2024-05-24 23:49:23 +0000

    automated ormolu commit

--- src/AOC/Y2023/Day7.hs
+++ src/AOC/Y2023/Day7.hs
@@ -1,26 +1,28 @@
 module AOC.Y2023.Day7 where
 
 import AOC.Y2021.Day24 (integer)
-import Control.Arrow((&&&))
+import Control.Arrow ((&&&))
+import Data.List (sortOn)
 import Data.Maybe
-import Data.List(sortOn)
-import Data.Ord(comparing)
+import Data.Ord (comparing)
 import Library
 
-data CamelCards a = CamelCards {
-  hand :: a,
-  bid :: Integer
-} deriving (Show, Functor)
+data CamelCards a = CamelCards
+  { hand :: a,
+    bid :: Integer
+  }
+  deriving (Show, Functor)
 
 camelCardsParser = CamelCards <$> hands <*> integer
 
 main_pt1 = do
   t <- lines <$> readFileY 2023 "d7.input"
   let (Right p) = sequence $ parse camelCardsParser "" <$> t
-  let res = zipWith (\i CamelCards{..} -> i * bid) [1..] (sortOn hand $ (handType <$>) <$> p)
+  let res = zipWith (\i CamelCards {..} -> i * bid) [1 ..] (sortOn hand $ (handType <$>) <$> p)
   return $ sum res
 
-hands = do {x <- many1 (digit <|> upper); spaces; return x}
+hands = do x <- many1 (digit <|> upper); spaces; return x
+
 handType = uncurry (flip rankCard) . ((toRank <$>) &&& (group . sort))
 
 data Hand a = HighCard a | OnePair a | TwoPair a | ThreeOfAKind a | FullHouse a | FourOfAKind a | FiveOfAKind a
@@ -38,10 +40,10 @@ toRank d = C1 d
 rankCard (crumple -> [(5, s)]) = FiveOfAKind
 rankCard (crumple -> [(1, s), (4, s')]) = FourOfAKind
 rankCard (crumple -> [(2, s), (3, s')]) = FullHouse
-rankCard (crumple -> [(1, s), (1,s'), (3, s'')]) = ThreeOfAKind
+rankCard (crumple -> [(1, s), (1, s'), (3, s'')]) = ThreeOfAKind
 rankCard (crumple -> [(1, s), (2, s'), (2, s'')]) = TwoPair
 rankCard (crumple -> [(1, s), (1, s'), (1, s''), (2, _)]) = OnePair
 rankCard (crumple -> [(1, s), (1, s'), (1, s''), (1, t), (1, t')]) = HighCard
 
-crumple :: Eq a => [[a]] -> [(Int, [a])]
-crumple =  sortOn fst <$> map (length &&& nub)
+crumple :: (Eq a) => [[a]] -> [(Int, [a])]
+crumple = sortOn fst <$> map (length &&& nub)
