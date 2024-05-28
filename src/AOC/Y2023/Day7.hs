b6b11088a0d3163ccd70023451ce530cec137b43
chris norris <cnorris2007@gmail.com>
2024-05-28 18:17:37 +0100

    Day 7

--- src/AOC/Y2023/Day7.hs
+++ src/AOC/Y2023/Day7.hs
@@ -7,28 +7,32 @@ import Data.Maybe
 import Data.Ord (comparing)
 import Library
 
+main_pt1 = main handType
+main_pt2 = main handType2
+
 data CamelCards a = CamelCards
   { hand :: a,
     bid :: Integer
-  }
-  deriving (Show, Functor)
-
-camelCardsParser = CamelCards <$> hands <*> integer
-
-main_pt1 = do
-  t <- lines <$> readFileY 2023 "d7.input"
-  let (Right p) = sequence $ parse camelCardsParser "" <$> t
-  let res = zipWith (\i CamelCards {..} -> i * bid) [1 ..] (sortOn hand $ (handType <$>) <$> p)
-  return $ sum res
-
-hands = do x <- many1 (digit <|> upper); spaces; return x
-
-handType = uncurry (flip rankCard) . ((toRank <$>) &&& (group . sort))
+  } deriving (Show, Functor)
 
 data Hand a = HighCard a | OnePair a | TwoPair a | ThreeOfAKind a | FullHouse a | FourOfAKind a | FiveOfAKind a
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Functor)
 
 data Rank a = C1 a | T | J | Q | K | A deriving (Eq, Ord, Show)
+
+camelCardsParser = 
+  CamelCards
+  <$> do x <- many1 (digit <|> upper); spaces; return x
+  <*> integer
+
+readCards = ((sequence <$>) . pure . ( parse camelCardsParser "" <$>)) =<< (lines <$> readFileY 2023 "d7.input")
+
+main h = do
+  (Right p) <- readCards
+  return $ sum (results h p)
+
+results h p =  zipWith (\i CamelCards {..} -> i * bid) [1 ..] (sortOn hand $ (h <$>) <$> p)
+handType = uncurry (flip rankCard) . ((toRank <$>) &&& (group . sort))
 
 toRank 'T' = T
 toRank 'J' = J
@@ -37,6 +41,22 @@ toRank 'K' = K
 toRank 'A' = A
 toRank d = C1 d
 
+toRank2 J = C1 '1'
+toRank2 x = x
+
+crumple :: (Eq a) => [[a]] -> [(Int, [a])]
+crumple = sortOn fst <$> map (length &&& nub)
+
+swapJ _ acc [] = handType acc
+swapJ c acc (s:st) = 
+  case s of 
+   'J' -> swapJ c ( c:acc) st
+   _   -> swapJ c ( s:acc) st
+
+handType2 card =
+  let newHandType = maximum [swapJ c [] card | c <- "TJQKA123456789"]
+  in (toRank2 . toRank <$> card) <$ newHandType
+
 rankCard (crumple -> [(5, s)]) = FiveOfAKind
 rankCard (crumple -> [(1, s), (4, s')]) = FourOfAKind
 rankCard (crumple -> [(2, s), (3, s')]) = FullHouse
@@ -44,6 +64,3 @@ rankCard (crumple -> [(1, s), (1, s'), (3, s'')]) = ThreeOfAKind
 rankCard (crumple -> [(1, s), (2, s'), (2, s'')]) = TwoPair
 rankCard (crumple -> [(1, s), (1, s'), (1, s''), (2, _)]) = OnePair
 rankCard (crumple -> [(1, s), (1, s'), (1, s''), (1, t), (1, t')]) = HighCard
-
-crumple :: (Eq a) => [[a]] -> [(Int, [a])]
-crumple = sortOn fst <$> map (length &&& nub)
