c767f71293dfadab15676ebcee9a203313afdb6f
chrisnorris <chrisnorris@users.noreply.github.com>
2024-05-28 17:18:05 +0000

    automated ormolu commit

--- src/AOC/Y2023/Day7.hs
+++ src/AOC/Y2023/Day7.hs
@@ -8,30 +8,33 @@ import Data.Ord (comparing)
 import Library
 
 main_pt1 = main handType
+
 main_pt2 = main handType2
 
 data CamelCards a = CamelCards
   { hand :: a,
     bid :: Integer
-  } deriving (Show, Functor)
+  }
+  deriving (Show, Functor)
 
 data Hand a = HighCard a | OnePair a | TwoPair a | ThreeOfAKind a | FullHouse a | FourOfAKind a | FiveOfAKind a
   deriving (Show, Eq, Ord, Functor)
 
 data Rank a = C1 a | T | J | Q | K | A deriving (Eq, Ord, Show)
 
-camelCardsParser = 
+camelCardsParser =
   CamelCards
-  <$> do x <- many1 (digit <|> upper); spaces; return x
-  <*> integer
+    <$> do x <- many1 (digit <|> upper); spaces; return x
+    <*> integer
 
-readCards = ((sequence <$>) . pure . ( parse camelCardsParser "" <$>)) =<< (lines <$> readFileY 2023 "d7.input")
+readCards = ((sequence <$>) . pure . (parse camelCardsParser "" <$>)) =<< (lines <$> readFileY 2023 "d7.input")
 
 main h = do
   (Right p) <- readCards
   return $ sum (results h p)
 
-results h p =  zipWith (\i CamelCards {..} -> i * bid) [1 ..] (sortOn hand $ (h <$>) <$> p)
+results h p = zipWith (\i CamelCards {..} -> i * bid) [1 ..] (sortOn hand $ (h <$>) <$> p)
+
 handType = uncurry (flip rankCard) . ((toRank <$>) &&& (group . sort))
 
 toRank 'T' = T
@@ -48,14 +51,14 @@ crumple :: (Eq a) => [[a]] -> [(Int, [a])]
 crumple = sortOn fst <$> map (length &&& nub)
 
 swapJ _ acc [] = handType acc
-swapJ c acc (s:st) = 
-  case s of 
-   'J' -> swapJ c ( c:acc) st
-   _   -> swapJ c ( s:acc) st
+swapJ c acc (s : st) =
+  case s of
+    'J' -> swapJ c (c : acc) st
+    _ -> swapJ c (s : acc) st
 
 handType2 card =
   let newHandType = maximum [swapJ c [] card | c <- "TJQKA123456789"]
-  in (toRank2 . toRank <$> card) <$ newHandType
+   in (toRank2 . toRank <$> card) <$ newHandType
 
 rankCard (crumple -> [(5, s)]) = FiveOfAKind
 rankCard (crumple -> [(1, s), (4, s')]) = FourOfAKind
