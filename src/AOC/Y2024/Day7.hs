54817f76aab4c521dc002affd55fb9d489181770
chris norris <cnorris2007@gmail.com>
2024-12-08 00:44:31 +0000

    Day 7 pt 2

--- src/AOC/Y2024/Day7.hs
+++ src/AOC/Y2024/Day7.hs
@@ -6,10 +6,18 @@ import qualified Data.List.Class as L
 import Library
 
 main_pt1 = do
-  input' <- lines <$> parseLists
-  let asCalibrations = sequence (parse parseCalibrations "" <$> input') ^. _Right
+  inp <- asCalibrations
   return $
-    sum [r | (r, i) <- asCalibrations, r `elem` run i]
+    sum [r | (r, i) <- inp, r `elem` run i [(+), (*)]]
+
+main_pt2 = do
+  inp <- asCalibrations
+  return $
+    sum [r | (r, i) <- inp, r `elem` run i [(+), (*), (|~)]]
+
+asCalibrations = do
+  input' <- lines <$> parseLists
+  return $ sequence (parse parseCalibrations "" <$> input') ^. _Right
 
 parseLists = readFileY 2024 "d7.input"
 
@@ -21,7 +29,9 @@ parseCalibrations = do
   n <- numbers
   return (i, n)
 
-run i@(l : ls) =
+run i@(l : ls) s =
   let m = length i
-      opTree = L.take (m - 1) (repeatM [(+), (*)])
+      opTree = L.take (m - 1) (repeatM s)
    in foldl' (\b (a, op) -> op b a) l <$> (zip ls <$> L.toList opTree)
+
+(|~) a b = let s = floor $ logBase 10 (fromIntegral b) in ((10^ (s+1)) * a) + b 
